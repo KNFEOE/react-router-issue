5d4976368ab367b123d99c3e47e7a3bfc0a9a92e
Mountain Wu <mountainwu@kuainiugroup.com>
2025-06-19 20:00:16 +0800

    chore: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { FileBasedRouterVite } from "../../src/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), FileBasedRouterVite(), tailwindcss()],
+	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "src"),
